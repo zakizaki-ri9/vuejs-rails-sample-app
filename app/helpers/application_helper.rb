1a2075f61f6669a84ef450762ebe8f6a47ab19ca
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-20 08:24:13 +0900

    pagination label no unless

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     def get_bootstrap_pageitem_class(is_first_or_last)
         if is_first_or_last
-            "page-item disable"
+            "page-item disabled"
         else
             "page-item"
         end
