3d8ad8c2b1e57d67bef13eba4e6c05c91b67b675
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-20 08:16:41 +0900

    add kaminari pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def get_bootstrap_pageitem_class(is_first_or_last)
+        if is_first_or_last
+            "page-item disable"
+        else
+            "page-item"
+        end
+    end
 end
